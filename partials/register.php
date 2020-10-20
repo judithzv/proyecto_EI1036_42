3584e1d816717e38180ea5d0b4582c7c9343ff9c
nuria <al375864@uji.es>
2020-10-20 18:58:51 +0200

    css registro

--- partials/register.php
+++ partials/register.php
@@ -1,29 +1,31 @@
 <main>
-	<h1>Registrar</h1>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<div id="container">
+<div id="mincontainer">
+	<b><h1>Registrar</h1></b>
 	<form class="fom_usuario" action="?action=registrar" method="POST">
-
 		<legend>Datos básicos</legend>
-		<label for="nombre">Nombre</label>
+		<label for="nombre" class="fa fa-user"><b> Nombre</b></label>
 		<br/>
 		<input type="text" name="name" class="item_requerid" size="20" maxlength="40">
 		<br/>
-        <label for="apellidos">Apellidos</label>
+        <label for="apellidos" class="fa fa-user-o"><b> Apellidos</b></label>
 		<br/>
 		<input type="text" name="surnames" class="item_requerid" size="20" maxlength="40">
 		<br/>
-        <label for="usuario">Usuario</label>
+        <label for="usuario" class="fa fa-sign-in" ><b> Usuario</b></label>
 		<br/>
 		<input type="text" name="username" class="item_requerid" size="20" maxlength="20">
 		<br/>
-        <label for="passwd">Contraseña</label>
+        <label for="passwd" class="fa fa-key" ><b> Contraseña</b></label>
 		<br/>
-		<input type="password" name="password" class="item_requerid" size="8" maxlength="20">
+		<input type="password" name="password" class="item_requerid" size="20" maxlength="20">
 		<br/>
-		<label for="email">Email</label>
+		<label for="email" class="fa fa-envelope"><b> Email</b></label>
 		<br/>
 		<input type="text" name="mail" class="item_requerid" size="20" maxlength="40">
 		<br/>
-        <label for="direccion">Dirección</label>
+        <label for="direccion" class="fa fa-address-card-o"><b> Dirección</b></label>
 		<br/>
 		<input type="text" name="address" class="item_requerid" size="20" maxlength="100">
 		<br/>
@@ -31,4 +33,6 @@
 		<input type="reset" value="Deshacer">
 		</p>
 	</form>
+</div>
+</div>
 </main>
