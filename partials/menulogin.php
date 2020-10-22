a49cf86641eba16969cd185e66c65783902dd0c0
nuria <al375864@uji.es>
2020-10-22 15:20:40 +0200

    margen

--- partials/menulogin.php
+++ partials/menulogin.php
@@ -1,6 +1,9 @@
 <main>
+<nav>
 		<a href="./portal.php?action=home" id="nm"><i class="fa fa-info-circle" aria-hidden="true"></i></a>
 		<a href="?action=productos" id="nm"><i class="fa fa-home" aria-hidden="true"></i></a>
 		<a href='?action=carrito' id="nm"><i class="fa fa-shopping-cart"></i></a>
 		<a href="?action=login" id="lnm"><i class="fa fa-sign-out" aria-hidden="true"></i></i></a>
+</nav>
+<nav id="margen"></nav>
 </main>
--- partials/menusinlogin.php
+++ partials/menusinlogin.php
@@ -1,6 +1,9 @@
 <main>
+	<nav>
 		<a href="./portal.php?action=home" id="nm"><i class="fa fa-info-circle" aria-hidden="true"></i></a>
 		<a href="?action=productos" id="nm"><i class="fa fa-home" aria-hidden="true"></i></a>
         <a href="?action=registro" id="nm"><i class="fa fa-user-plus" aria-hidden="true"></i></a>
 		<a href="?action=login" id="lnm"><i class="fa fa-sign-in" aria-hidden="true"></i></a>
+	</nav>
+<nav id="margen"></nav>
 </main>
